3d3dde55ca7735e6d265bded5d46921e85614e2d
M. Yavuz YAGIS <myagis16@ku.edu.tr>
2020-11-06 23:50:33 +0300

    final update: fix needed game engine does not work..

--- js/app.js
+++ js/app.js
@@ -1,61 +1,29 @@
 
 // Global Variables
-const rockPlayed = document.querySelector('#rock');
-const scissorsPlayed = document.querySelector('#scissors');
-const paperPlayed = document.querySelector('#paper');
-let result = document.querySelector('.result').textContent;
-// Player Variables:
+let result = document.querySelector('.result').innerText;
+let aiselect;
 let playerselect;
+// Player Variables:
 let ai_upper;
+const buttons = document.querySelectorAll('.btn')
 
 // score variables:
-let tie = document.querySelector('.tie').textContent;
-let ai = document.querySelector('.ai').textContent;
-let user = document.querySelector('.user').textContent;
+let tie = document.querySelector('.tie').innerText;
+let ai = document.querySelector('.ai').innerText;
+let user = document.querySelector('.user').innerText;
 
 
-// Player Selection!
-function userplay(){
-    rockPlayed.addEventListener('click', ()=>{
-        playerselect=game('rock',aiselect())
-        console.log(playerselect)
-
+// user selections
+buttons.forEach((button) =>{button.addEventListener('click',()=>{
+    playerselect = button.id;
+    console.log(playerselect)
+    aiselection()
+    console.log(aiselect)
     })
+})
 
-    scissorsPlayed.addEventListener('click',()=>{
-        playerselect=game('scissors',aiselect())
-        
-    })
-
-    paperPlayed.addEventListener('click', ()=>{
-        playerselect=game('paper',aiselect())
-    })
-}
-// AI GAME WORKS!
-let aiselect=()=>{
-    let answer =  Math.floor(Math.random()* (3) +1);
-    if (answer==1) {
-        ai_upper= 'Rock!'
-        document.querySelector('.sub-header').textContent=`AI selected ${ai_upper}`
-        return 'rock';
-    } else if(answer==2) {
-        ai_upper='Scissors!'
-        document.querySelector('.sub-header').textContent=`AI selected ${ai_upper}`
-        return 'scissors'
-    }else if(answer==3){
-        ai_upper = 'Paper!'
-        document.querySelector('.sub-header').textContent=`AI selected ${ai_upper}`
-        return 'paper'
-    }
-    document.getElementById('result').style.fontFamily="sans"
-    document.getElementById('result').style.fontSize="3rem"
-}
-
-
-//Game Logic 
-
-function game(playerselect,aiselect){
-    if(user<5 && ai<5){
+function game(playerselect, aiselect){
+    if(user < 5 && ai < 5 ){
         if(playerselect==aiselect){
             result = 'It is a Tie!'
             tie = tie+1
@@ -86,4 +54,29 @@ function game(playerselect,aiselect){
     }
 }
 
-game(playerselect, aiselect)
+
+
+// AI GAME WORKS!
+function aiselection(){
+    let answer =  Math.floor(Math.random()* (3) +1);
+    if (answer==1) {
+        ai_upper= 'Rock!'
+        document.querySelector('.sub-header').textContent=`AI selected ${ai_upper}`
+        aiselect= 'rock';
+        
+    } else if(answer==2) {
+        ai_upper='Scissors!'
+        document.querySelector('.sub-header').textContent=`AI selected ${ai_upper}`
+        aiselect= 'scissors'
+    }else if(answer==3){
+        ai_upper = 'Paper!'
+        document.querySelector('.sub-header').textContent=`AI selected ${ai_upper}`
+        aiselect= 'paper'
+    }
+    document.getElementById('result').style.fontFamily="sans"
+    document.getElementById('result').style.fontSize="3rem"
+}
+
+
+//Game Logic 
+
